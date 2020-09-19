19ecc9b34be53f1f80e7113712703fa659776edc
Akash Adhikari <akashadhikari470@gmail.com>
2020-09-19 11:01:51 +0545

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class ="header">
+@section('header')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Akash App</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">blog<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="list">userlist</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="account"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+
+</body>
+</html>
+
+@show
+</div>
+<div class ="content">
+@section('content')
+@show
+</div>
+<div class ="footer">
+@section('footer')
+<h1>footer part</h1>
+@show
+</div>
+    
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+<h1> login</h1>
+@endsection
